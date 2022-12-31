7d3937121192c7c106ff2c6dcf6ab2734ff129d9
madcow95 <lobasketve@naver.com>
2022-12-31 22:37:58 +0900

    Initial Commit

---
+++ firstiOS/firstiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstiOSApp.swift
+//  firstiOS
+//
+//  Created by ChoiKwangWoo on 2022/12/31.
+//
+
+import SwiftUI
+
+@main
+struct firstiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
